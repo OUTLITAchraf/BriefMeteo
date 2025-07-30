9e6413051149570a6874d87670efb22133212e42
OUTLITAchraf <outlitachraf5@gmail.com>
2025-07-30 21:27:14 +0100

    afficer le météo de recherche city

--- script.js
+++ script.js
@@ -4,6 +4,123 @@ let cityInput = document.getElementById("city-input");
 let todayWeatherElements = document.querySelector("#todayWeather");
 let api_key = "561d905e67fc60afecba2387e4877853";
 
+function getWeatherData(name, lat, lon, country, state) {
+  let WEATHER_API_URL = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${api_key}`;
+
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
+
+  fetch(WEATHER_API_URL)
+    .then((res) => res.json())
+    .then((data) => {
+      let date = new Date();
+
+      function formatTime(unixTimestamp) {
+        const date = new Date(unixTimestamp * 1000);
+        const hours = date.getHours().toString().padStart(2, "0");
+        const minutes = date.getMinutes().toString().padStart(2, "0");
+        return `${hours}:${minutes}`;
+      }
+
+      todayWeatherElements.innerHTML = `
+          <h1 class="italic font-bold text-6xl text-[#FFD700] mt-12">
+            Meteo ${name} , ${country}
+          </h1>
+          <div class="flex gap-4 mt-8 ml-10">
+            <img src="https://openweathermap.org/img/wn/${
+              data.weather[0].icon
+            }@2x.png" alt="" width="200" height="200" />
+            <div class="ml-5">
+              <p class="text-2xl font-semibold mt-5">
+                ${days[date.getDay()]} ${date.getDate()} ${
+        months[date.getMonth()]
+      } ${date.getFullYear()}
+              </p>
+              <p class="text-6xl font-extrabold my-5">${(
+                data.main.temp - 273.15
+              ).toFixed(2)}&deg;C</p>
+              <p class="text-2xl font-bold">${data.weather[0].description}</p>
+            </div>
+          </div>
+          <div class="flex gap-10 mt-10 ml-10">
+            <div class="flex gap-5 mt-10 mr-5">
+              <img src="./Img/icons/humidity.png" alt="" width="70" height="70" />
+              <div>
+                <p class="font-bold text-2xl">Humidity</p>
+                <p class="font-bold text-2xl">${data.main.humidity} %</p>
+              </div>
+            </div>
+            <div class="flex gap-5 mt-10">
+              <img
+                src="./Img/icons/gauge.png"
+                alt=""
+                width="70"
+                height="70"
+              />
+              <div>
+                <p class="font-bold text-2xl">Pressure</p>
+                <p class="font-bold text-2xl">${data.main.pressure} hPA</p>
+              </div>
+            </div>
+            <div class="flex gap-5 mt-10 ml-5">
+              <img src="./Img/icons/wind (2).png" alt="" width="70" height="70" />
+              <div>
+                <p class="font-bold text-2xl">Wind Speed</p>
+                <p class="font-bold text-2xl">${data.wind.speed} m/s</p>
+              </div>
+            </div>
+          </div>
+          <div class="flex gap-10 mt-10 ml-10">
+            <div class="flex gap-5 mt-10 mr-5">
+              <img
+                src="./Img/icons/sunrise.png"
+                alt=""
+                width="70"
+                height="70"
+              />
+              <div>
+                <p class="font-bold text-2xl">Sunrise</p>
+                <p class="font-bold text-2xl">${formatTime(
+                  data.sys.sunrise
+                )}</p>
+              </div>
+            </div>
+            <div class="flex gap-5 mt-10">
+              <img src="./Img/icons/sunset.png" alt="" width="70" height="70" />
+              <div>
+                <p class="font-bold text-2xl">Sunset</p>
+                <p class="font-bold text-2xl">${formatTime(data.sys.sunset)}</p>
+              </div>
+            </div>
+          </div>
+        `;
+    })
+    .catch(() => {
+      alert("Error fetching weather data. Please try again later.");
+    });
+  }
+
 function getCityCoordinates() {
   let cityName = cityInput.value.trim();
   cityInput.value = "";
@@ -12,7 +129,8 @@ function getCityCoordinates() {
   fetch(GEOCODING_API_URL)
     .then((res) => res.json())
     .then((data) => {
-      console.log(data);
+      let { name, lat, lon, country, state } = data[0];
+      getWeatherData(name, lat, lon, country, state);
     })
     .catch(() => {
       alert(
